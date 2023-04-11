402e8973fb0f5f0384c980c5ae624ce6545da88d
chendy tri wardani <83945155+chendytriwardani@users.noreply.github.com>
2023-04-12 04:01:03 +0700

    footer aja

--- resources/views/layouts/frontend/footer.blade.php
+++ resources/views/layouts/frontend/footer.blade.php
@@ -1,11 +1,78 @@
 <footer id="footer" {{-- data-aos="fade-up" data-aos-easing="ease-in-out" data-aos-duration="100" --}}>
 
+<<<<<<< Updated upstream
   {{-- <div class="footer-newsletter">
     <div class="container">
       <div class="row">
         <div class="col-lg-6">
           <h4>Our Newsletter</h4>
           <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna</p>
+=======
+{{-- <div class="footer-newsletter">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-6">
+        <h4>Our Newsletter</h4>
+        <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna</p>
+      </div>
+      <div class="col-lg-6">
+        <form action="" method="post">
+          <input type="email" name="email"><input type="submit" value="Subscribe">
+        </form>
+      </div>
+    </div>
+  </div>
+</div> --}}
+
+<div class="footer-top">
+  <div class="container">
+    <div class="row">
+
+      <div class="col-lg-3 col-md-6 footer-links">
+        <h4>Menu</h4>
+        <ul>
+          <li><i class="bx bx-chevron-right"></i> <a href="{{ route('dashboard.index') }}">Home</a></li>
+          <li><i class="bx bx-chevron-right"></i> <a href="{{ route('fasilitas.show') }}">Fasilitas</a></li>
+          <li><i class="bx bx-chevron-right"></i> <a href="{{ route('blog.index') }}">Berita</a></li>
+          <li><i class="bx bx-chevron-right"></i> <a href="{{ route('gallery.show') }}">Geleri</a></li>
+          <li><i class="bx bx-chevron-right"></i> <a href="{{ route('about') }}">Tentang</a></li>
+        </ul>
+      </div>
+
+      <div class="col-lg-6 col-md-6 footer-links">
+        <h4>Profile</h4>
+        <ul>
+          <li><i class="bx bx-chevron-right"></i> <a href="{{ route('yayasan') }}">Profil yayasan</a></li>
+          <li><i class="bx bx-chevron-right"></i> <a href="{{ route('sd') }}">SD Tri Guna Bhakti</a></li>
+          <li><i class="bx bx-chevron-right"></i> <a href="{{ route('smp') }}">SMP Tri Guna Bhakti</a></li>
+          <li><i class="bx bx-chevron-right"></i> <a href="{{ route('smk') }}">SMK Tri Guna Bhakti</a></li>
+        </ul>
+      </div>
+      
+      <div class="col-lg-3 col-md-6 footer-contact">
+        <h4>Contact Us</h4>
+        <p>
+          Jl. Kyai Tambak Deres No.34, Bulak <br>
+          Kec. Bulak, Kota SBY, Jawa Timur <br>
+          60124 <br><br>
+          <strong>Phone:</strong>  031 - 51500309<br>
+          <strong>Email:</strong> yayasantrigunabhakti@gmail.com<br><br>
+          <strong>Social Media: </strong> <br> 
+          <strong><a style="padding-right:10px;" href="https://instagram.com/trigunabhakti.ofc?igshid=YmMyMTA2M2Y="> <i style="font-size:22px;" class="fa-brands fa-instagram"></i></a></strong> 
+          <strong><a style="padding-right:10px;" href="#"> <i style="font-size:22px;" class="fa-brands fa-twitter"></i></a></strong>  
+          <strong><a style="padding-right:10px;" href="https://www.facebook.com/profile.php?id=100089912815887"> <i style="font-size:22px;" class="fa-brands fa-facebook"></i></a></strong>          </p>
+
+      </div>
+
+      {{-- <div class="col-lg-3 col-md-6 footer-info">
+        <h3>About Moderna</h3>
+        <p>Cras fermentum odio eu feugiat lide par naso tierra. Justo eget nada terra videa magna derita valies darta donna mare fermentum iaculis eu non diam phasellus.</p>
+        <div class="social-links mt-3">
+          <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+          <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+          <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+          <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+>>>>>>> Stashed changes
         </div>
         <div class="col-lg-6">
           <form action="" method="post">
